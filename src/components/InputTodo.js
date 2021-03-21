12a4031d04d84db65446d54d43ecd56396135758
Farna <lukagizo@gmail.com>
2021-03-22 00:57:21 +0400

    update

---
+++ src/components/InputTodo.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+class InputTodo extends Component {
+  state = {
+    title: '',
+  };
+
+  onChange = (e) => {
+    this.setState({ title: e.target.value });
+  };
+
+  onSubmit = (e) => {
+    e.preventDefault();
+
+    if (this.state.title.trim()) {
+      this.props.addTodoItem(this.state.title);
+      this.setState({
+        title: '',
+      });
+    } else alert('Please write item');
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.onSubmit}>
+        <input
+          type="text"
+          placeholder="Add Todo..."
+          value={this.state.title}
+          onChange={this.onChange}
+        />
+        <button>Submit</button>
+      </form>
+    );
+  }
+}
+export default InputTodo;
